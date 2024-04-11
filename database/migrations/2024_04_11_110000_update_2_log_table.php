8db369de9ca26802c1e7391cf445e7eb6bfa4ec5
PradoMarquina <lprado@broobe.com>
2024-04-11 00:04:10 -0300

    fix

--- database/migrations/2024_04_11_110000_update_2_log_table.php
+++ database/migrations/2024_04_11_110000_update_2_log_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('logs', function (Blueprint $table) {
       
             $table->string('message')->nullable();
            
@@ -23,7 +23,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('logs', function (Blueprint $table) {
 
             $table->dropColumn('message');
         });
