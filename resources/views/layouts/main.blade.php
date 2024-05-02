1697fe5724378819a95fb959da1cea6b82ce81fa
PradoMarquina <lprado@broobe.com>
2024-05-01 23:51:36 -0300

    cambios los cdn por archivos js y css icon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,10 +11,15 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> --}}
         {{-- incons bootrap --}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"> --}}
+    
+    {{-- reempalzo cdn icon y min css --}}
+    <link href="{{ asset('/css/bootstrap-icons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet">
+    
     <!-- Scripts -->
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     <link rel="icon" href="{{ asset('/img/logo_co.ico') }}">
@@ -59,9 +64,9 @@
     @stack('modals')
    
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+    </script> --}}
     <script src="https://kit.fontawesome.com/252b802fc2.js" crossorigin="anonymous"></script>
     <script>
         window.INIT_MIN_MINUTE_CHATTER = `{{ env('INIT_MIN_MINUTE_CHATTER') }}`;
@@ -74,6 +79,8 @@
         
         <script src="{{ asset('/js/mobiscroll.javascript.min.js') }}"></script>
         <script src="{{ asset('/js/custom.js') }}"></script>
+        {{-- agrego por cdn bundel js remplazo --}}
+        <script src="{{ asset('/js/bootstrap.bundle.min.js') }}"></script>
         @stack('schedule')
         @stack('chatters')
         @stack('copyText')
