fe7efc877c4ad67493b6d3d820b7a0f262adad47
PradoMarquina <lprado@broobe.com>
2024-07-04 23:47:31 -0300

    Admin swicht grupos por plataforma

--- resources/views/livewire/support/support-user.blade.php
+++ resources/views/livewire/support/support-user.blade.php
@@ -49,7 +49,7 @@
                             allowfullscreen>
                         </iframe>
                     </div>
-                    <div class="col" style="margin-top: 100px !important;">
+                    <div class="col" style="margin-top: 50px !important;">
                         <iframe id="twitch-chat-embed"
                                 src="https://www.twitch.tv/embed/aquino/chat?parent=www.comunidadnc.com"
                                 height="500"
