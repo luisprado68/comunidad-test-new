5719381953f1a4c519f1bef35ff471e4d71a287c
PradoMarquina <lprado@broobe.com>
2024-08-06 14:08:07 -0300

    Se modifica logs

--- resources/views/admin/nav.blade.php
+++ resources/views/admin/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-light bg-light justify-content-between">
     <a class="navbar-brand">Admin</a>
     <form class="form-inline">
-      
-        <a href="{{ route('logout-admin') }}"><button type="button" class="btn btn-danger">Log out</button></a>
+
+{{--        <a href="{{ route('logout-admin') }}"><button type="button" class="btn btn-danger">Log out</button></a>--}}
     </form>
   </nav>
