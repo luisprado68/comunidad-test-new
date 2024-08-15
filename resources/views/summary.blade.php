6eebe7223c3a69d9185dfe185f07cd1188653fd6
PradoMarquina <lprado@broobe.com>
2024-08-15 20:45:09 -0300

    se agrega validaciones

--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -96,12 +96,12 @@
 
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script>
-        const toastLiveExample = document.getElementById('liveToast');
-        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
-        toastBootstrap.show();
-    </script>
+{{--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>--}}
+{{--    <script>--}}
+{{--        const toastLiveExample = document.getElementById('liveToast');--}}
+{{--        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);--}}
+{{--        toastBootstrap.show();--}}
+{{--    </script>--}}
 @endsection
 @push('chatters')
 
