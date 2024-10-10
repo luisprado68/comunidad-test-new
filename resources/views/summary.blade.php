a74dd5223f6830cd0a482dd5c29ccd4d1cb2337b
PradoMarquina <lprado@broobe.com>
2024-10-09 21:04:45 -0300

    cambios de la rama nuevo-contador-dev dev-contador

--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -95,21 +95,11 @@
 
         </div>
     </div>
-{{--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>--}}
-{{--    <script>--}}
-{{--        const toastLiveExample = document.getElementById('liveToast');--}}
-{{--        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);--}}
-{{--        toastBootstrap.show();--}}
-{{--    </script>--}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        const toastLiveExample = document.getElementById('liveToast');
+        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+        toastBootstrap.show();
+    </script>
 @endsection
-@push('chatters')
 
-    {{-- @if (env('APP_ENV') == 'local')
-        <script src="{{ asset('/js/setTime.js') }}"></script>
-    @else
-        <script src="./public/js/setTime.js"></script>
-    @endif --}}
-
-
-
-@endpush
