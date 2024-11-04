43c9ed1b1ae77fbbc11b73e64ec43ca69a135b97
PradoMarquina <lprado@broobe.com>
2024-11-04 09:39:31 -0300

    se agrega vista de summary youtube

--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -20,72 +20,79 @@
 
                 @endif
                 @include('status', ['user' => $user])
-                <div class="pt-1 mb-4 col-md-12 w-100">
-                    <div class="card bg-dark">
-                        <div class="card-body ">
-                            <div class="row">
-                                <div class="col-lg-6 col-md-6 col-sm-12">
-                                    <div class="card banner-border">
-                                        <div class="card-body banner">
-                                            <h3 class="text-center text-light">Rango</h3>
+
+                @if($user->platform_id == \App\Enums\PlatformType::google)
+                        @include('summary_youtube',['user'=>$user])
+                    @else
+                        {{--                Todo pasar a componente--}}
+                        <div class="pt-1 mb-4 col-md-12 w-100">
+                            <div class="card bg-dark">
+                                <div class="card-body ">
+                                    <div class="row">
+                                        <div class="col-lg-6 col-md-6 col-sm-12">
+                                            <div class="card banner-border">
+                                                <div class="card-body banner">
+                                                    <h3 class="text-center text-light">Rango</h3>
+                                                </div>
+
+                                            </div>
+                                            <div class="text-center col justify-content-center">
+                                                {{-- @if (env('APP_ENV') == 'local') --}}
+                                                @if ($user->range->id == 1 )
+                                                    <img src="{{ asset('/img/BRONCE.png') }}" alt="tag" class="m-2 w-50">
+                                                @elseif($user->range->id == 2 )
+                                                    <img src="{{ asset('/img/PLATA.png') }}" alt="tag" class="m-2 w-50">
+                                                @elseif($user->range->id == 3 )
+                                                    <img src="{{ asset('/img/ORO.png') }}" alt="tag" class="m-2 w-50">
+                                                @elseif($user->range->id == 4 )
+                                                    <img src="{{ asset('/img/PLATINO.png') }}" alt="tag" class="m-2 w-50">
+                                                @elseif($user->range->id == 5 )
+                                                    <img src="{{ asset('/img/ESMERALDA.png') }}" alt="tag" class="m-2 w-50">
+                                                @elseif($user->range->id == 6 )
+                                                    <img src="{{ asset('/img/DIAMANTE.png') }}" alt="tag" class="m-2 w-50">
+                                                @elseif($user->range->id == 7 )
+                                                    <img src="{{ asset('/img/HIERRO.png') }}" alt="tag" class="m-2 w-50">
+                                                @elseif($user->range->id == 8 )
+                                                    <img src="{{ asset('/img/LIDERES.png') }}" alt="tag" class="m-2 w-50">
+                                                @endif
+
+                                                {{-- @else
+                                                    @if ($user->range->id == 1 )
+                                                        <img src="./public/img/bronce.svg" alt="tag" class="m-2 w-50">
+                                                    @elseif($user->range->id == 2 )
+                                                        <img src="./public/img/plata.svg" alt="tag" class="m-2 w-50">
+                                                    @elseif($user->range->id == 3 )
+                                                        <img src="./public/img/oro.svg" alt="tag" class="m-2 w-50">
+                                                    @elseif($user->range->id == 4 )
+                                                        <img src="./public/img/platino.svg" alt="tag" class="m-2 w-50">
+                                                    @endif
+                                                @endif --}}
+                                                {{-- <h4 class="text-center text-light">{{$user->range->name}}</h4> --}}
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6 col-md-6 col-sm-12">
+                                            <div class="mb-3 card banner-border">
+                                                <div class="card-body banner">
+                                                    <h3 class="text-center text-light">Referidos</h3>
+                                                </div>
+                                            </div>
+                                            <div class="text-center col justify-content-center">
+                                                @if (count($ref)> 0)
+                                                    @foreach ( $ref as $item)
+                                                        <h3 class="mb-1 text-center text-light">{{$item}}</h3>
+                                                    @endforeach
+                                                @endif
+
+
+                                            </div>
                                         </div>
 
                                     </div>
-                                    <div class="text-center col justify-content-center">
-                                        {{-- @if (env('APP_ENV') == 'local') --}}
-                                            @if ($user->range->id == 1 )
-                                                <img src="{{ asset('/img/BRONCE.png') }}" alt="tag" class="m-2 w-50">
-                                            @elseif($user->range->id == 2 )
-                                                <img src="{{ asset('/img/PLATA.png') }}" alt="tag" class="m-2 w-50">
-                                            @elseif($user->range->id == 3 )
-                                                <img src="{{ asset('/img/ORO.png') }}" alt="tag" class="m-2 w-50">
-                                            @elseif($user->range->id == 4 )
-                                                <img src="{{ asset('/img/PLATINO.png') }}" alt="tag" class="m-2 w-50">
-                                            @elseif($user->range->id == 5 )
-                                                <img src="{{ asset('/img/ESMERALDA.png') }}" alt="tag" class="m-2 w-50">
-                                            @elseif($user->range->id == 6 )
-                                                <img src="{{ asset('/img/DIAMANTE.png') }}" alt="tag" class="m-2 w-50">
-                                            @elseif($user->range->id == 7 )
-                                                <img src="{{ asset('/img/HIERRO.png') }}" alt="tag" class="m-2 w-50">
-                                            @elseif($user->range->id == 8 )
-                                                <img src="{{ asset('/img/LIDERES.png') }}" alt="tag" class="m-2 w-50">
-                                            @endif
-
-                                        {{-- @else
-                                            @if ($user->range->id == 1 )
-                                                <img src="./public/img/bronce.svg" alt="tag" class="m-2 w-50">
-                                            @elseif($user->range->id == 2 )
-                                                <img src="./public/img/plata.svg" alt="tag" class="m-2 w-50">
-                                            @elseif($user->range->id == 3 )
-                                                <img src="./public/img/oro.svg" alt="tag" class="m-2 w-50">
-                                            @elseif($user->range->id == 4 )
-                                                <img src="./public/img/platino.svg" alt="tag" class="m-2 w-50">
-                                            @endif
-                                        @endif --}}
-                                        {{-- <h4 class="text-center text-light">{{$user->range->name}}</h4> --}}
-                                    </div>
                                 </div>
-                                <div class="col-lg-6 col-md-6 col-sm-12">
-                                    <div class="mb-3 card banner-border">
-                                        <div class="card-body banner">
-                                            <h3 class="text-center text-light">Referidos</h3>
-                                        </div>
-                                    </div>
-                                    <div class="text-center col justify-content-center">
-                                        @if (count($ref)> 0)
-                                            @foreach ( $ref as $item)
-                                                <h3 class="mb-1 text-center text-light">{{$item}}</h3>
-                                            @endforeach
-                                        @endif
-
-
-                                    </div>
-                                </div>
-
                             </div>
                         </div>
-                    </div>
-                </div>
+                @endif
+
                 @if (!isset($user_watched))
                     @include('share')
                 @endif
---
+++ resources/views/summary_youtube.blade.php
@@ -0,0 +1,74 @@
+<div class="pt-1 mb-4 col-md-12 w-100">
+    <div class="card bg-dark">
+        <div class="card-body ">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="card banner-border">
+                        <button type="button" class="btn btn-primary">Compra neocoins <i class="bi bi-bag-fill"></i></button>
+                    </div>
+                    <div class="card banner-border mt-2">
+                        <div class="card-body bg-light rounded">
+                            <h5 class="text-center text-dark">aaaaaaaaaaaaaaaaaaaaaaaaaaaa</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="card banner-border">
+                        <button type="button" class="btn btn-primary">Gana neocoins gratis <i class="bi bi-coin"></i></button>
+                    </div>
+                    <div class="card banner-border mt-2">
+                        <div class="card-body bg-light rounded">
+                            <h5 class="text-center text-dark">aaaaaaaaaaaaaaaaaaaaaaaaaaaa</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="card banner-border">
+                    <button type="button" class="btn btn-primary">Crear una oferta</button>
+                    </div>
+                    <div class="card banner-border mt-2">
+                        <div class="card-body bg-light rounded">
+                            <h5 class="text-center text-dark">aaaaaaaaaaaaaaaaaaaaaaaaaaaa</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 mt-4">
+                    <div class="card-body banner">
+                        <h3 class="text-center text-light">Tu actividad reciente</h3>
+                    </div>
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th scope="col">Fecha</th>
+                            <th scope="col">Descripción</th>
+                            <th scope="col">Neocoins</th>
+                            <th scope="col">Balance</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">11/10/24</th>
+                            <td>mov</td>
+                            <td>200</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                        </tbody>
+
+                    </table>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
