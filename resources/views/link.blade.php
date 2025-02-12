b78a4bfd931da9045f971431621327b57d789568
PradoMarquina <lprado@broobe.com>
2025-02-11 21:03:54 -0300

    se actualiza link de filtro

--- resources/views/link.blade.php
+++ resources/views/link.blade.php
@@ -8,7 +8,7 @@
                 <p>Ingresa a nuestro grupo de filtro y noticias, lee las reglas y sigue todas las indicaciones, en este
                     grupo tendrás todas las noticias de la comunidad como colaboraciones, torneos, eventos y más. </p>
                 <div class="col-12">
-                    <a href="https://chat.whatsapp.com/DpGoC5cOEL3EkXfIue1IPM" target="_blank"><button
+                    <a href="https://chat.whatsapp.com/BOhubmaXQ7N94MqUHuXQYA" target="_blank"><button
                             class="btn bg-success col-12 " style="color:#00a884">
                             <i class="bi bi-whatsapp" style="color:#fff"></i>
                         </button></a>
